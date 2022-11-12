651cd88ae8a27de991beea0cfc3e6a3250bc5780
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2022-11-12 16:36:38 -0500

    Introduce ember_can_callbacks to decouple from cuber-base

---
+++ ember-can/ember_can_callbacks.h
@@ -0,0 +1,9 @@
+#ifndef EMBER_CAN_CALLBACKS_H
+#define EMBER_CAN_CALLBACKS_H
+
+/**
+ * ember-can will call this function when CAN communication is lost.
+ */
+void ember_can_callback_notify_lost_can(void);
+
+#endif
