be89096f841f94cbe751ee6288d6c373cfa2b538
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-01-19 20:38:16 -0500

    Integrate `opencan` into `ember-can`, replaces `can_gen`/cantools

--- ember-can/ember_can.c
+++ ember-can/ember_can.c
@@ -1,12 +1,17 @@
-#include "ember_can.h"
 #include "ember_can_callbacks.h"
 
-#include <esp_ota_ops.h>
-#include <driver/twai.h>
 #include <stdio.h>
+#include <string.h>
+
+#include <esp_ota_ops.h>
+#include <esp_timer.h>
+#include <driver/twai.h>
 
 #include "common.h"
 #include "ember_taskglue.h"
+#include "opencan_callbacks.h"
+#include "opencan_rx.h"
+#include "opencan_tx.h"
 
 // ######        DEFINES        ###### //
 
@@ -15,14 +20,9 @@
 
 // ######      PROTOTYPES       ###### //
 
-static void can_send_msg(const twai_message_t *message);
-
 // ######     PRIVATE DATA      ###### //
 
 static bool can_ready;
-
-static can_incoming_t *in_msgs[25];
-static uint in_msgs_count = 0;
 
 // ######    RATE FUNCTIONS     ###### //
 
@@ -36,9 +36,9 @@ ember_rate_funcs_S can_rf = {
 
 static void can_init()
 {
-    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(CAN_TX_GPIO, CAN_RX_GPIO, TWAI_MODE_NORMAL);
-    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
-    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+    const twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(CAN_TX_GPIO, CAN_RX_GPIO, TWAI_MODE_NORMAL);
+    const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+    const twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
 
     if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
         can_ready = 1;
@@ -53,69 +53,59 @@ static void can_init()
     }
 }
 
-
+// RX and TX for CAN. RX needs to be split off into a separate task.
 static void can_1kHz()
 {
-    // update time deltas
-    for (uint i = 0; i < in_msgs_count; i++) {
-        in_msgs[i]->delta_ms += 1;
-    }
+    /* Run TX scheduler */
+    CANTX_scheduler_1kHz();
 
+    /**
+     * RX
+     *
+     * Loop trying twai_recieve() until we run out of messages to process.
+     *
+     * todo: this is synchronous at 1kHz, when it really should be asynchronous
+     */
     for (;;) {
-
-        twai_message_t msg;
-        esp_err_t result = twai_receive(&msg, 0);
+        twai_message_t message;
+        const esp_err_t result = twai_receive(&message, 0);
 
         switch (result) {
             case ESP_ERR_TIMEOUT: // no messages in queue
                 return;
 
             case ESP_OK:
-                for (uint i = 0; i < in_msgs_count; i++) {
-                    if (in_msgs[i]->id == msg.identifier) {
-                        in_msgs[i]->unpack(in_msgs[i]->out, msg.data, msg.data_length_code);
-                        in_msgs[i]->delta_ms = 0;
-                        in_msgs[i]->recieved = true;
-                    }
-                }
+                CANRX_handle_rx(message.identifier, message.data, message.data_length_code);
                 break;
 
-            default:
-                return; // error
+            default: // error
+                return;
         }
     }
 }
 
 // ######   PRIVATE FUNCTIONS   ###### //
 
-static void can_send_msg(const twai_message_t *message)
-{
-    esp_err_t r = twai_transmit(message, 0);
+// ######   PUBLIC FUNCTIONS    ###### //
 
+void CAN_callback_enqueue_tx_message(const uint8_t * const data, const uint8_t len, const uint32_t id) {
+    /* prepare twai_message_t */
+    twai_message_t message = {
+        .identifier = id,
+        .extd = true, // todo
+        .data_length_code = len,
+    };
+
+    /* copy the provided data into the twai_message_t */
+    memcpy(message.data, data, len);
+
+    /* send/enqueue message */
+    const esp_err_t r = twai_transmit(&message, 0);
     if (r != ESP_OK) {
         ember_can_callback_notify_lost_can();
-        // attempt recovery?
     }
 }
 
-// ######   PUBLIC FUNCTIONS    ###### //
-
-void can_register_incoming_msg(can_incoming_t *cfg)
-{
-    in_msgs[in_msgs_count] = cfg;
-    in_msgs_count++;
+uint64_t CAN_callback_get_system_time(void) {
+    return esp_timer_get_time();
 }
-
-void can_send_iface(const can_outgoing_t *i, const void *s)
-{
-    twai_message_t msg = {
-        .identifier = i->id,
-        .extd = i->extd,
-        .data_length_code = i->dlc,
-    };
-
-    i->pack(msg.data, s, 8);
-
-    can_send_msg(&msg);
-}
-
