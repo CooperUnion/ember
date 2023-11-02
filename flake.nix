5496596995c122c76bfc6f37754355b73c4ae514
Jacob Koziej <jacobkoziej@gmail.com>
2023-11-02 13:05:54 -0400

    Add `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Ember";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nixpkgs-fmt
+          ];
+        };
+      }
+    );
+}
